75e09d82d2101f9fc83c090a1b51a6a019bdc931
Tradition Learning Community <traditionlearningcommunity@gmail.com>
2026-07-27 13:55:18 +0200

    chore(handoff): remove unused validator staging file

--- tools/handoff/validate_handoff_core.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Validate progressive Feature Handoff Package v1.0 populations."""
-
-from __future__ import annotations
-
-# This file is populated from the previous validator blob in the next commit.
